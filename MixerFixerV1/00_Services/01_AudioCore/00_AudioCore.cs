acb06b7585ee50d2ab9d9de83750a1195a1ab7c2
NickSoftlutions <69628132+NickSoftlutions@users.noreply.github.com>
2023-01-27 19:25:57 +0200

    Bug Fixes

--- MixerFixerV1/00_Services/01_AudioCore/00_AudioCore.cs
+++ MixerFixerV1/00_Services/01_AudioCore/00_AudioCore.cs
@@ -80,12 +80,14 @@ namespace Services
 
         public void LoadPriorityList()
         {
-            foreach (var L_Device in G_MMDeviceEnumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.All).OrderBy(d => d.DeviceFriendlyName))
+            //MMDeviceCollection L_Devices = G_MMDeviceEnumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.All);
+
+            foreach (var L_Device in G_MMDeviceEnumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.All).Where(d => d.DeviceFriendlyName != null).OrderBy(d => d.DeviceFriendlyName))
             {
                 G_Srv_DB.DevicePriority_GetOneOrAdd(L_Device.FriendlyName, LoadPriorityList_Get_DisplayText(L_Device), L_Device.DeviceFriendlyName, false);
             }
 
-            foreach (var L_Device in G_MMDeviceEnumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.All).OrderBy(d => d.DeviceFriendlyName))
+            foreach (var L_Device in G_MMDeviceEnumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.All).Where(d => d.DeviceFriendlyName != null).OrderBy(d => d.DeviceFriendlyName))
             {
                 G_Srv_DB.DevicePriority_GetOneOrAdd(L_Device.FriendlyName, LoadPriorityList_Get_DisplayText(L_Device), L_Device.DeviceFriendlyName, true);
             }
